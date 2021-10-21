2720656bbaa39bd8a2e53de0bae907cd43d08259
TarkaB <tarka.hayden.williams@gmail.com>
2021-10-21 14:35:06 +0100

    Finish setup

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -3,9 +3,12 @@ use engine::{
     render::{colour, Console, Pixel},
 };
 
+const SCREEN_WIDTH: u16 = 80;
+const SCREEN_HEIGHT: u16 = 30;
+
 fn main() {
-    let mut console =
-        Console::create(80, 30, 16, 16, "Example Game").expect("Could not create Console");
+    let mut console = Console::create(SCREEN_WIDTH, SCREEN_HEIGHT, 16, 16, "TETRIS")
+        .expect("Could not create Console");
     let mut keyboard =
         Keyboard::create(vec![Key::UP, Key::DOWN, Key::LEFT, Key::RIGHT, Key::CHAR_Z]);
 
