4b5193352146d520ea46be3180d761f6567d1e4d
TarkaB <tarka.hayden.williams@gmail.com>
2021-10-21 14:31:53 +0100

    Add main game loop

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use engine::{
+    input::{Key, Keyboard},
+    render::{colour, Console, Pixel},
+};
+
+fn main() {
+    let mut console =
+        Console::create(80, 30, 16, 16, "Example Game").expect("Could not create Console");
+    let mut keyboard =
+        Keyboard::create(vec![Key::UP, Key::DOWN, Key::LEFT, Key::RIGHT, Key::CHAR_Z]);
+
+    let mut game_active = true;
+
+    while game_active {
+        // TIMING //////////
+
+        // INPUT //////////
+        keyboard.update_key_states();
+
+        // GAME LOGIC //////////
+
+        // RENDER //////////
+        console
+            .update_screen()
+            .expect("Could not update the screen");
+    }
+}
