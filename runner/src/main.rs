56d03b21a7e1481908af232b92585ffa623dafad
TarkaB <tarka.hayden.williams@gmail.com>
2021-10-23 00:11:28 +0100

    Add Game of Life

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
+use game_of_life;
 use tetris;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
         println!("You need to specify a game to run.");
@@ -12,6 +12,7 @@ fn main() {
     let game_name = args[1].as_str();
 
     match game_name.to_lowercase().as_str() {
+        "gol" => game_of_life::run(),
         "tetris" => tetris::run(),
         _ => println!("Not a valid game name."),
     }
