a9733f71b9151c07214b408809ddca90cb55bf04
TarkaB <tarka.hayden.williams@gmail.com>
2021-10-23 00:19:55 +0100

    Setup

--- game_of_life/src/lib.rs
+++ game_of_life/src/lib.rs
@@ -1,1 +1,28 @@
-pub fn run() {}
+use engine::{
+    input::{Key, Keyboard},
+    render::{self, colour, Console, Pixel},
+};
+
+const SCREEN_WIDTH: u16 = 80;
+const SCREEN_HEIGHT: u16 = 45;
+
+pub fn run() {
+    let mut console = Console::create(SCREEN_WIDTH, SCREEN_HEIGHT, 16, 16, "CONWAY'S GAME OF LIFE")
+        .expect("Failed to create Console");
+    let mut keyboard = Keyboard::create(vec![Key::ESCAPE, Key::CHAR_Q]);
+
+    loop {
+        // TICK //////////
+
+        // INPUT //////////
+        keyboard.update_key_states();
+
+        // GAME LOGIC //////////
+        if keyboard.get_key_state(Key::ESCAPE).is_pressed() {
+            break;
+        }
+
+        // RENDER //////////
+        console.update_screen().expect("Failed to update screen");
+    }
+}
