71c2f2879f0bb4ca76eb1036be1be21069c75f62
TarkaB <tarka.hayden.williams@gmail.com>
2021-10-23 00:04:53 +0100

    Finish `runner`

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
 use tetris;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("You need to specify a game to run.");
+        std::process::exit(0);
+    }
+
+    let game_name = args[1].as_str();
+
+    match game_name.to_lowercase().as_str() {
+        "tetris" => tetris::run(),
+        _ => println!("Not a valid game name."),
+    }
+}
